ed830a50935a7abc244525d4996fead3fc80422e
Rayaan Khan <rayaan.k.ca@gmail.com>
2023-04-02 04:50:26 -0400

    added dict and json capability

--- Scrape.py
+++ Scrape.py
@@ -4,6 +4,7 @@ the individual quote time stamps, stores them as individual .mp3 file and it's a
 """
 
 #imports
+import json
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -28,8 +29,8 @@ search_bar = driver.find_element(by='id', value='video_url')
 search_bar.send_keys('https://www.youtube.com/watch?v=MkKxAi_c0LQ')
 search_bar.send_keys(Keys.ENTER)
 
-# wait 5 seconds
-time.sleep(3) 
+# wait 3 seconds
+time.sleep(2) 
 
 # step 2: grab the time stamps and text
 demo_div = driver.find_element(By.ID, 'demo') # locate div with the element id
@@ -41,65 +42,54 @@ elements = demo_div.find_elements(By.CSS_SELECTOR, '*')
 current_string = ''
 quotes = []
 
-start_time = []
-end_time = []
+quote_data = {}
 
 br_count = 0
 beg = True
 
-
 # loop through the elements
 for element in elements:
-    # print(element.tag_name)
 
     if element.tag_name == 'a':
         br_count = 0
         current_string += element.text + ' '
         if beg == True:
-            start_time.append(element.get_attribute('data-start'))
-            # print("Start Time: ", element.get_attribute('data-start'))
+            quote_data['start_time'] = element.get_attribute('data-start')
 
         ending = element.get_attribute('data-end')
 
     elif element.tag_name == 'br':
         br_count+=1
         beg = False
-        # print("inc. br by 1")
 
         if br_count == 2:
-            # print("BR = 2")
-            quotes.append(current_string)
-            # print('final string: ', current_string)
+            quote_data['quote'] = current_string.strip()
+            quote_data['end_time'] = ending
+            quotes.append(quote_data)
+            quote_data = {}
             current_string = ''
             br_count = 0
-            # print('br reset')
             beg = True
-            end_time.append(ending)
-            # print("End time: ", ending, "\n")
 
     else:
         br_count = 0
             
 # Add the last string to the list if not already added
 if current_string.strip() != '':
-
-    
-    quotes.append(current_string)
-    end_time.append(element.get_attribute('data-end'))
-
-
-# Print the list of strings
-count = 0
-for quote in quotes:
-    print(f"TimeStamp: [{start_time[count]}, {end_time[count]}] {quote}")
-    print("---------")
-    count += 1
-    
-    
-
-
-# step 3:
-
+    quote_data['quote'] = current_string.strip()
+    quote_data['end_time'] = element.get_attribute('data-end')
+    quotes.append(quote_data)
 
 # Close the browser window
 driver.quit()
+
+# Print the list of strings
+# count = 0
+# for quote in quotes:
+#     print(f"TimeStamp: [{start_time[count]}, {end_time[count]}] {quote}")
+#     print("---------")
+#     count += 1
+
+# Write the dictionary to a JSON file
+with open('quotes.json', 'w') as f:
+    json.dump(quotes, f, indent=2)
